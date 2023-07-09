3ce17695c2b763e2467d482edf06af42b61d34bc
Lucas B. S <125415413+LucasBS4546@users.noreply.github.com>
2023-07-09 12:56:37 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var kiryus = 0
 
 var repetidor1 = 0
 
-function atualizarValor() {
+function atualizarValorVisual() {
 
     var buttonValue = document.getElementById("value");
 
@@ -31,7 +31,7 @@ function eventosValor() {
 
         var buttonStart = document.createElement('button');
         buttonStart.innerText = 'Iniciar jogo (10 kiryus)';
-        buttonStart.onclick = function() {iniciarJogo()};
+        buttonStart.onclick = function() {iniciarJogo(10)};
         buttonStart.id = 'botaoIniciar'
 
         var areaInicial = document.getElementById("area");
@@ -44,17 +44,116 @@ function eventosValor() {
 }
 
 
-function KiryuClick(quantidadeClick) {
+function AlterarValor(quantidadeClick) {
 
     kiryus = kiryus + quantidadeClick;
 
-    atualizarValor();
+    atualizarValorVisual();
     eventosValor();
 
 }
 
 
-function comprarAlgo(custoItem) {
+function iniciarJogo(custoItem) {
+
+    if(kiryus < custoItem){
+        
+        alert('Pobre')
+        
+    } else {
+        
+        AlterarValor(-custoItem);
+        var botaoIniciar = document.getElementById("botaoIniciar");
+        botaoIniciar.remove();
+    
+        document.getElementById("column1").style.display = "flex";
+        document.getElementById("column3").style.display = "flex";
+        
+    }
+}
+
+
+
+//Loja
+
+function itemUm(custoItem) {
+
+    if(kiryus < custoItem){
+        
+        alert('Pobre')
+        
+    } else {
+        
+        AlterarValor(-custoItem);
+        
+    }
+
+}
+
+function itemDois(custoItem) {
+
+    if(kiryus < custoItem){
+        
+        alert('Pobre')
+        
+    } else {
+        
+        AlterarValor(-custoItem);
+        
+    }
+
+}
+
+function itemTres(custoItem) {
+
+    if(kiryus < custoItem){
+        
+        alert('Pobre')
+        
+    } else {
+        
+        AlterarValor(-custoItem);
+        
+    }
+
+}
+
+
+
+//Upgrades
+
+var upgrade3comprado = false;
+var upgrade4comprado = false;
+
+function desativarBotaoUpgrade(numeroid) {
+    var botaoAtual = document.getElementById("Bupgrade" + numeroid);
+    botaoAtual.disabled = true;
+    botaoAtual.className = "desativado";
+}
+
+function upgrade1(custoItem) {
+
+    if(kiryus < custoItem){
+        
+        alert('Pobre')
+        
+    } else {
+        
+        AlterarValor(-custoItem);
+
+        desativarBotaoUpgrade(1);
+
+        document.getElementById("linha2").style.opacity = "1";
+
+        var proxBotao = document.getElementById("Bupgrade2");
+        proxBotao.onclick = function() {upgrade2(25)}
+  
+    }
+
+}
+
+
+function upgrade2(custoItem) {
     
     if(kiryus < custoItem){
         
@@ -62,41 +161,88 @@ function comprarAlgo(custoItem) {
         
     } else {
         
-        KiryuClick(-custoItem);
+        AlterarValor(-custoItem);
+
+        desativarBotaoUpgrade(2);
+
+        document.getElementById("linha3").style.opacity = "1";
+
+        var proxBotao = document.getElementById("Bupgrade3");
+        proxBotao.onclick = function() {upgrade3(25)}
+
+        var proxBotao = document.getElementById("Bupgrade4");
+        proxBotao.onclick = function() {upgrade4(25)}
+
+    }
+
+}
+
+function upgrade3(custoItem) {
+    
+    if(kiryus < custoItem){
+        
+        alert('Pobre')
+        
+    } else {
+        
+        AlterarValor(-custoItem);
+
+        desativarBotaoUpgrade(3);
+
+        upgrade3comprado = true;
+
+        if(upgrade3comprado && upgrade4comprado) {
+
+            document.getElementById("linha4").style.opacity = "1";
+
+            var proxBotao = document.getElementById("Bupgrade5");
+            proxBotao.onclick = function() {upgrade5(25)}
+
+        }
         
     }
     
+}
+
+function upgrade4(custoItem) {
     
-}
+    if(kiryus < custoItem){
+        
+        alert('Pobre')
+        
+    } else {
+        
+        AlterarValor(-custoItem);
 
+        desativarBotaoUpgrade(4);
 
-function iniciarJogo() {
+        upgrade4comprado = true;
 
-    comprarAlgo(10);
+        if(upgrade3comprado && upgrade4comprado) {
+            
+            document.getElementById("linha4").style.opacity = "1";
 
-    var botaoIniciar = document.getElementById("botaoIniciar");
-    botaoIniciar.remove();
-
-    document.getElementById("column1").style.display = "flex";
-    document.getElementById("column3").style.display = "flex";
-
-}
-
-
-function itemUm() {
-
-    comprarAlgo(1);
-
-}
-
-function itemDois() {
-
-    comprarAlgo(3);
+            var proxBotao = document.getElementById("Bupgrade5");
+            proxBotao.onclick = function() {upgrade5(25)}
+            
+        }
+        
+    }
 
 }
 
-function itemTres() {
+function upgrade5(custoItem) {
+    
+    if(kiryus < custoItem){
+        
+        alert('Pobre')
+        
+    } else {
+        
+        AlterarValor(-custoItem);
 
-    comprarAlgo(5);
-
+        desativarBotaoUpgrade(5);
+        
+    }
+    
 }
