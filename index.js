4ca674b04c2dfe14c44cf04d42e00bac5605ab42
Lucas B. S <125415413+LucasBS4546@users.noreply.github.com>
2023-07-09 21:42:14 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var kiryus = 0
 
 var repetidor1 = 0
 
+var kps = 0;
+
+var clickValor = 1
+
+var percentualKpsClick = 0
+
+var secaoFala = document.getElementById('caixaDeFala');
+
+
 function atualizarValorVisual() {
 
     var buttonValue = document.getElementById("value");
@@ -17,6 +26,22 @@ function atualizarValorVisual() {
     }else {
 
         buttonValue.innerHTML = kiryus + ' Kiryus';
+
+    }
+
+}
+
+function atualizarKpsVisual() {
+
+    var kpsvalue = document.getElementById("valuekps");
+
+    if(kps == 69){
+
+        kpsvalue.innerHTML = kps + ' Nice :)';
+
+    }else {
+
+        kpsvalue.innerHTML = kps + ' kps';
 
     }
 
@@ -38,15 +63,33 @@ function eventosValor() {
         areaInicial.appendChild(buttonStart);
 
         repetidor1 = 1;
-        console.log(repetidor1)
+
     }
 
 }
 
+function KiryuClick(numeroClick) {
 
-function AlterarValor(quantidadeClick) {
+    if(percentualKpsClick > 0) {
 
-    kiryus = kiryus + quantidadeClick;
+        var valorPercent = Math.floor(kps/percentualKpsClick)
+        kiryus = kiryus + numeroClick + valorPercent;
+
+    } else {
+
+        kiryus = kiryus + numeroClick;
+        
+    }
+
+    atualizarValorVisual();
+    eventosValor();
+
+}
+
+
+function AlterarValor(quantidade) {
+
+    kiryus = kiryus + quantidade;
 
     atualizarValorVisual();
     eventosValor();
@@ -69,6 +112,16 @@ function iniciarJogo(custoItem) {
         document.getElementById("column1").style.display = "flex";
         document.getElementById("column3").style.display = "flex";
         
+
+        setInterval(function () {
+
+            atualizarKpsVisual()
+            AlterarValor(kps)
+
+        }, 1000);
+
+
+        AlterarValor(1000)
     }
 }
 
@@ -76,7 +129,11 @@ function iniciarJogo(custoItem) {
 
 //Loja
 
-function itemUm(custoItem) {
+var qntItem1 = 0
+var qntItem2 = 0
+var qntItem3 = 0
+
+function item1(custoItem) {
 
     if(kiryus < custoItem){
         
@@ -85,12 +142,29 @@ function itemUm(custoItem) {
     } else {
         
         AlterarValor(-custoItem);
+
+        kps = kps + 1;
+        atualizarKpsVisual()
+
+        qntItem1++;
+
+        var novoValor = Math.floor(custoItem*1.8)
+
+        var botaoAtual = document.getElementById("Bitem1")
+
+        botaoAtual.onclick = function() {item1(parseFloat(novoValor))}
+
+        var texto = document.getElementById("H4item1")
+        texto.innerText = novoValor + " Kiryus"
+
+        var textoQnt = document.getElementById("H5item1")
+        textoQnt.innerText = qntItem1;
         
     }
 
 }
 
-function itemDois(custoItem) {
+function item2(custoItem) {
 
     if(kiryus < custoItem){
         
@@ -99,12 +173,29 @@ function itemDois(custoItem) {
     } else {
         
         AlterarValor(-custoItem);
-        
+
+        kps = kps + 5;
+        atualizarKpsVisual()
+
+        qntItem2++;
+
+        var novoValor = Math.floor(custoItem*1.8)
+
+        var botaoAtual = document.getElementById("Bitem2")
+
+        botaoAtual.onclick = function() {item2(parseFloat(novoValor))}
+
+        var texto = document.getElementById("H4item2")
+        texto.innerText = novoValor + " Kiryus"
+
+        var textoQnt = document.getElementById("H5item2")
+        textoQnt.innerText = qntItem2;
+
     }
 
 }
 
-function itemTres(custoItem) {
+function item3(custoItem) {
 
     if(kiryus < custoItem){
         
@@ -113,6 +204,25 @@ function itemTres(custoItem) {
     } else {
         
         AlterarValor(-custoItem);
+
+        kps = kps + 20;
+        atualizarKpsVisual()
+
+        qntItem3++;
+
+        
+        var novoValor = Math.floor(custoItem*1.8)
+
+        var botaoAtual = document.getElementById("Bitem3")
+
+        botaoAtual.onclick = function() {item3(parseFloat(novoValor))}
+
+        var texto = document.getElementById("H4item3")
+        texto.innerText = novoValor + " Kiryus"
+
+        
+        var textoQnt = document.getElementById("H5item3")
+        textoQnt.innerText = qntItem3;
         
     }
 
@@ -129,6 +239,7 @@ function desativarBotaoUpgrade(numeroid) {
     var botaoAtual = document.getElementById("Bupgrade" + numeroid);
     botaoAtual.disabled = true;
     botaoAtual.className = "desativado";
+    document.getElementById("H4upgrade" + numeroid).style.color = "lightgray"
 }
 
 function upgrade1(custoItem) {
@@ -146,7 +257,12 @@ function upgrade1(custoItem) {
         document.getElementById("linha2").style.opacity = "1";
 
         var proxBotao = document.getElementById("Bupgrade2");
-        proxBotao.onclick = function() {upgrade2(25)}
+        proxBotao.onclick = function() {upgrade2(50)}
+
+        document.getElementById("TextoArvoreEmCima").style.opacity = "1"
+
+        document.getElementById("Bupgrade1").style.display = "none"
+
   
     }
 
@@ -168,10 +284,14 @@ function upgrade2(custoItem) {
         document.getElementById("linha3").style.opacity = "1";
 
         var proxBotao = document.getElementById("Bupgrade3");
-        proxBotao.onclick = function() {upgrade3(25)}
+        proxBotao.onclick = function() {upgrade3(150)}
 
         var proxBotao = document.getElementById("Bupgrade4");
-        proxBotao.onclick = function() {upgrade4(25)}
+        proxBotao.onclick = function() {upgrade4(150)}
+
+        var botaoKiryu = document.getElementById("botaoKiryu")
+
+        clickValor = clickValor + 1;
 
     }
 
@@ -196,9 +316,11 @@ function upgrade3(custoItem) {
             document.getElementById("linha4").style.opacity = "1";
 
             var proxBotao = document.getElementById("Bupgrade5");
-            proxBotao.onclick = function() {upgrade5(25)}
+            proxBotao.onclick = function() {upgrade5(1)}
 
         }
+
+        percentualKpsClick = percentualKpsClick + 10;
         
     }
     
@@ -223,9 +345,11 @@ function upgrade4(custoItem) {
             document.getElementById("linha4").style.opacity = "1";
 
             var proxBotao = document.getElementById("Bupgrade5");
-            proxBotao.onclick = function() {upgrade5(25)}
+            proxBotao.onclick = function() {upgrade5(1)}
             
         }
+
+        clickValor = clickValor + 3;
         
     }
 
@@ -243,6 +367,218 @@ function upgrade5(custoItem) {
 
         desativarBotaoUpgrade(5);
         
+        document.getElementById("linha5").style.opacity = "1";
+
+        var proxBotao = document.getElementById("Bupgrade6");
+        proxBotao.onclick = function() {upgrade6(50)}
+        
     }
     
+}
+
+function upgrade6(custoItem) {
+    
+    if(kiryus < custoItem){
+        
+        alert('Pobre')
+        
+    } else {
+        
+        AlterarValor(-custoItem);
+
+        desativarBotaoUpgrade(6);
+
+        document.getElementById("inserirKiryus").style.display = "flex";
+        
+        dialogoUm()
+    }
+    
+}
+
+
+
+// Dialogo
+
+function dialogoUm() {
+
+    var elementoTextoP = document.createElement('p');
+    elementoTextoP.id = 'falaKiryu';
+    var iFala = 0;
+    var txt = 'Olá, sou eu, o Kiryu do PCC. O que você deseja comigo?'; 
+    var velocidade = 50; 
+
+    secaoFala.appendChild(elementoTextoP);
+
+    function typeWriter() {
+        if (iFala < txt.length) {
+        elementoTextoP.innerHTML += txt.charAt(iFala);
+        iFala++;
+        setTimeout(typeWriter, velocidade);
+        }
+    }
+    typeWriter();
+    
+}
+
+function resposta1() {
+
+    secaoFala.removeChild(secaoFala.firstChild);
+
+    var elementoTextoP = document.createElement('p');
+    elementoTextoP.id = 'falaKiryu';
+    var iFala = 0;
+    var txt = 'Que assim seja, irei lhe conceder seu desejo. Apenas digite quantos Kiryus deseja, e você irá os receber.'; 
+    var velocidade = 50; 
+
+    secaoFala.appendChild(elementoTextoP);
+
+    function typeWriter() {
+        if (iFala < txt.length) {
+        elementoTextoP.innerHTML += txt.charAt(iFala);
+        iFala++;
+        setTimeout(typeWriter, velocidade);
+        }
+    }
+    typeWriter();
+
+    document.getElementById("caixaDeFala").style.fontSize = "30px";
+
+    document.getElementById("opcao2").style.display = "none"
+    document.getElementById("opcao1").style.display = "none"
+
+    document.getElementById("inputFala").style.opacity = "1"
+    document.getElementById("inputFala").style.width = "80%"
+
+
+}
+
+
+function resposta2() {
+
+    secaoFala.removeChild(secaoFala.firstChild);
+
+    var elementoTextoP = document.createElement('p');
+    elementoTextoP.id = 'falaKiryu';
+    var iFala = 0;
+    var txt = 'Eu sou um ser inalcançável, acima de qualquer existência. Tu jamais conseguirias viver lado a lado comigo. Infelizmente, nosso amor é proibido.'; 
+    var velocidade = 50; 
+
+    secaoFala.appendChild(elementoTextoP);
+
+    function typeWriter() {
+        if (iFala < txt.length) {
+        elementoTextoP.innerHTML += txt.charAt(iFala);
+        iFala++;
+        setTimeout(typeWriter, velocidade);
+        }
+    }
+    typeWriter();
+
+    document.getElementById("caixaDeFala").style.fontSize = "24px";
+
+    var botaoResp = document.getElementById("opcao2")
+    botaoResp.innerText = "Mas eu te amo"
+    botaoResp.onclick = function() {resposta3()}
+
+    document.getElementById("opcao1").style.display = "none"
+
+}
+
+function resposta3() {
+
+    secaoFala.removeChild(secaoFala.firstChild);
+
+    var elementoTextoP = document.createElement('p');
+    elementoTextoP.id = 'falaKiryu';
+    var iFala = 0;
+    var txt = 'Se você insiste... eu conheço uma única forma de nos unirmos... de você realmente ter um relacionamento sério comigo... Você está disposto a passar por o que quer que seja para isso?'; 
+    var velocidade = 50; 
+
+    secaoFala.appendChild(elementoTextoP);
+
+    function typeWriter() {
+        if (iFala < txt.length) {
+        elementoTextoP.innerHTML += txt.charAt(iFala);
+        iFala++;
+        setTimeout(typeWriter, velocidade);
+        }
+    }
+    typeWriter();
+
+    document.getElementById("caixaDeFala").style.fontSize = "22px";
+
+    
+    
+    var botaoResp = document.getElementById("opcao2")
+    botaoResp.innerText = "Não"
+    botaoResp.onclick = function() {resposta4()}
+
+    document.getElementById("opcao1").style.display = "flex"
+
+    var botaoResp2 = document.getElementById("opcao1")
+    botaoResp2.innerText = "Sim"
+    botaoResp2.onclick = function() {resposta5()}
+
+}
+
+
+function resposta4() {
+
+    secaoFala.removeChild(secaoFala.firstChild);
+
+    var elementoTextoP = document.createElement('p');
+    elementoTextoP.id = 'falaKiryu';
+    var iFala = 0;
+    var txt = 'Neste caso não tenho nada para dizer para você.'; 
+    var velocidade = 50; 
+
+    secaoFala.appendChild(elementoTextoP);
+
+    function typeWriter() {
+        if (iFala < txt.length) {
+        elementoTextoP.innerHTML += txt.charAt(iFala);
+        iFala++;
+        setTimeout(typeWriter, velocidade);
+        }
+    }
+    typeWriter();
+
+    document.getElementById("opcao2").style.display = "none"
+
+    
+    var botaoResp2 = document.getElementById("opcao1")
+    botaoResp2.innerText = "Eu desejo muitos Kiryus"
+    botaoResp2.onclick = function() {resposta1()}
+
+}
+
+function resposta5() {
+
+    secaoFala.removeChild(secaoFala.firstChild);
+
+    var elementoTextoA = document.createElement('a');
+    elementoTextoA.id = 'falaKiryuA';
+    elementoTextoA.href = "https://store.steampowered.com/app/1088710/Yakuza_3_Remastered/"
+    elementoTextoA.target = "_blank"
+    var iFala = 0;
+    var txt = 'ENTÃO COMPRE YAKUZA 3 POR APENAS R$89,50! A EXPERIÊNCIA DO JOGO SERÁ DE TAMANHA QUALIDADE QUE SERÁ QUASE O MESMO QUE TER UM RELACIONAMENTO SÉRIO COM O PRÓPRIO KIRYU!!! '; 
+    var velocidade = 15; 
+
+    secaoFala.appendChild(elementoTextoA);
+
+    function typeWriter() {
+        if (iFala < txt.length) {
+        elementoTextoA.innerHTML += txt.charAt(iFala);
+        iFala++;
+        setTimeout(typeWriter, velocidade);
+        }
+    }
+    typeWriter();
+
+    document.getElementById("opcao2").style.display = "none"
+
+    var botaoResp = document.getElementById("opcao1")
+    botaoResp.innerText = "Não"
+    botaoResp.onclick = function() {resposta4()}
+
 }
